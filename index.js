04380e87354c4f09cb55ea7dc77e61bf0c20bef4
rocky <rocky.bernstein@consensys.net>
2019-06-14 22:20:29 -0400

    getTypedefinition and getDefinition
    Minially working.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 "use strict";
 
+// FIXME there has to be a better way.
+// Maybe we'll convert everything to typescript.
 const lspManager = require("./lib/lspManager");
+const helper = require("./lib/helper");
+
 module.exports.LspManager = lspManager.LspManager;
+module.exports.rangeToLspPosition = helper.rangeToLspPosition;
+module.exports.LspPositionToOffset = helper.LspPositionToOffset;
