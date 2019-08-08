d63b2f4f2ef9af7ccd58921f99cd5073ecda091d
rocky <rocky.bernstein@consensys.net>
2019-08-08 16:17:07 -0400

    More tests
    Beef up test makefile

--- tests/test-ast-fns.ts
+++ tests/test-ast-fns.ts
@@ -4,7 +4,7 @@ const fs = require("fs");
 import { SourceMappings } from "../out/solc-ast";
 import { SolcFileInfo } from "../out/ast-fns";
 import { StaticInfo } from "../out/gather-info";
-import { getDefinition, getTypeDefinition } from "../out";
+import { getDefinition, getTypeDefinition, getReferences } from "../out";
 
 tape("conversion", (t: tape.Test) => {
   const solidityAst = join(__dirname, "/resources/ast.json");
@@ -18,11 +18,15 @@ tape("conversion", (t: tape.Test) => {
     ast,
     sourceMapping: new SourceMappings(solidityStr),
     staticInfo,
-    content: "",
+    content: solidityStr,
   };
 
   t.test("AstFns", (st: tape.Test) => {
 
+    /* owner = msg.sender
+       ^^^^^
+       of token-good.sol is line 9 characters 8-13
+    */
     const range = {
       start: {
         line: 9,
@@ -42,7 +46,7 @@ tape("conversion", (t: tape.Test) => {
       }
     };
 
-    const typeInfo = getTypeDefinition(finfo, range);
+    let typeInfo = getTypeDefinition(finfo, range);
     if (typeInfo !== null) st.deepEqual(typeInfo.id, 5, `got right definition AST id`);
 
     // Do another with no range/
@@ -51,10 +55,56 @@ tape("conversion", (t: tape.Test) => {
     // const defInfo = lspMgr.gotoDefinition(solidityFile, range);
     // console.log(defInfo);
     // st.ok(defInfo);
-    const info = getDefinition(finfo, range);
+    let info = getDefinition(finfo, range);
     if (info !== null) st.equal(info.id, 5);
 
-    // st.equal(typeInfo, null, "type definition not found");
+    /* And when we can't find the dfinition... */
+    range.start.line = range.end.line = 1;
+    typeInfo = getTypeDefinition(finfo, range);
+    st.equal(typeInfo, null,
+             "Should return null when for typeDefinition when no AST node found")
+
+    info = getTypeDefinition(finfo, range);
+    st.equal(typeInfo, null,
+             "Should return null when for dfinition when no AST node found")
+
+    /* getReferences()... */
+    const refRange = {
+      start: {
+        line: 21,
+        character: 4
+      },
+      end: {
+        line: 21,
+        character: 18
+      },
+      active: {
+        line: 21,
+        character: 4
+      },
+      anchor: {
+        line: 21,
+        character: 18
+      }
+    };
+
+    let refInfo = getReferences(finfo, refRange);
+    if (refInfo) {
+      const got = refInfo.map(r => [r.id, r.nodeType]);
+      st.deepEqual(got,
+                   [ [ 33, 'Identifier' ],
+                     [ 44, 'Identifier' ],
+                     [ 54, 'Identifier' ] ],
+                   "getReferences of token-good.sol 'is_paused'")
+    } else {
+      st.ok(refInfo, "Should have found 3 references")
+    }
+
+    // And when we can't find the node... */
+    refInfo = getReferences(finfo, range);
+    st.equal(refInfo, null,
+             "Should return null for references when no AST node found ")
+
     st.end();
   });
 
